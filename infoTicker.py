52f2e83477ddf1e82f2839a9eff7be861a52a0bc
der-le <74149103+der-le@users.noreply.github.com>
2021-03-26 15:36:56 +0100

    Add files via upload

--- infoTicker.py
+++ infoTicker.py
@@ -1,16 +1,21 @@
-import FundamentalAnalysis as fa
+profile = fa.profile(ticker, api_key)
 
-api_key = 'e438c06772eb98390b1e07e0ab32b4bc'
-ticker = 'AAPL'
 
 elif input_main[0] == "info":
                 if len(input_main) == 2:
-                    print(rate(input_main[1]))
+                    print(info(input_main[1]))
                 else:
                     print('Geben Sie EIN Ticker Symbol hinter "info" ein.')
 
 
 def info(ticker):
-    profile = fa.profile(ticker, api_key)
-    return profile
+    name = profile["companyName"]
+    exchangeShortName = profile["exchangeShortName"]
+    sector = profile["sector"]
+    description = profile["description"]
+    fullTimeEmployees = profile["fullTimeEmployees"]
+    ceo = profile["ceo"]
+    address = profile["adress"]
+
+    return(info)
 
